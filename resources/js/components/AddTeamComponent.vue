5e8d8ae7ec4541715d95e3b20d878a1f157c9067
calbertomt <cmejiat@gmail.com>
2019-09-08 20:29:20 -0300

    ACTUALIZACION COMPONENTES VUE

---
+++ resources/js/components/AddTeamComponent.vue
@@ -0,0 +1,16 @@
+<template>
+	<button type="button" class="btn btn-primary top-space" data-toggle="modal" data-target="#addTeam">
+	  			New Player
+	</button>
+	<!-- <a href="" class="btn btn-primary top-space">New Player</a> -->
+</template>
+<script>
+	export default {
+
+	}
+</script>
+<style>
+	.top-space{
+		margin-top: 20px
+	}
+</style>
---
+++ resources/js/components/CreateTeamComponent.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="modal fade" id="addTeam" role="dialog" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Agregar Jugador</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+        	<form @submit.prevent="saveTeam">
+  	        <div class="form-group">
+    			    <label>Jugador</label>
+    			    <input type="text" class="form-control" placeholder="Ingresa el nombre del jugador" v-model="name">
+    		  	</div>
+            <div class="form-group">
+              <label>Posición</label>
+              <input type="text" class="form-control" placeholder="Ingresa la posición del jugador" v-model="position">
+            </div>
+    		  	<div class="form-group">
+    			    <label>Foto</label>
+    			    <input type="text" class="form-control" placeholder="Ingresa la url de una imagen" v-model="picture">
+    		  	</div>
+    		  	<button type="submit" class="btn btn-primary">Guardar</button>
+    	  	</form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default{
+    data(){
+      return{
+        name: null,
+        position: null,
+        picture: null
+      }
+    },
+    methods: {
+      saveTeam: function(){
+        console.log(this.name)
+        console.log(this.position)
+        console.log(this.picture)
+      }
+    }
+  }
+</script>
